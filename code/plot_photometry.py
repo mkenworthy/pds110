7210c93f58c5ea30a453b6d0d037792178e35e5e
Matthew Kenworthy <matthew.kenworthy@gmail.com>
2017-08-26 15:49:30 +0200

    reads aavsodata.txt only

--- code/plot_photometry.py
+++ code/plot_photometry.py
@@ -12,9 +12,7 @@ matplotlib.use('qt5agg')
 import matplotlib.pyplot as plt
 import seaborn as sns   
 
-
-aavso_file = '../data/aavsodata_598d6894a988c.txt'
-aavso_file = '../data/aavsodata_59962839569b7.txt'
+aavso_file = '../data/aavsodata.txt'
 
 ta = ascii.read(aavso_file)
 
@@ -37,12 +35,6 @@ ta_by_band = ta.group_by('Band')
 # the number of different bands
 n_bands = ta_by_band.groups.indices.size - 1
 
-fig, axes = plt.subplots(n_bands, 1, figsize=(8, 11), sharex=True)
-                         
-                         # subplot_kw={'xticks': [], 'yticks': []})
-
-fig.subplots_adjust(hspace=0.05, wspace=0.05)
-
 conv = {'HKEB':'s',
         'HMB':'o',
         'MGW':'v',
@@ -56,45 +48,26 @@ obscol = {'HKEB':'r',
         'MGAB':'m'}
 
 
-# loop over the bands
-for (ax, band) in zip(axes, ta_by_band.groups.keys):
-    print(band)
-    mask = ta_by_band.groups.keys['Band'] == band[0]
-
-    # loop over each band and estimate out of transit flux
-    tb = ta_by_band.groups[mask]
-
-    tb_by_obs = tb.group_by('Observer Code')
-    n_obs = tb_by_obs.groups.indices.size - 1
-    print(n_obs)
-
-#    ax.errorbar(tb['MJD'], tb['Magnitude'], tb['Uncertainty'], fmt='o')
-#    ax.text(0.1, 0.9, band[0], ha='center', va='center', transform=ax.transAxes)
-
-    tmax = 57970.
-
-    t_noecl = (tb['MJD'] < tmax)
-
-    # make an out of eclipse average
-    t_out = tb[t_noecl]
-
-    mean_mag = np.mean(t_out['Magnitude'])
-
-
-axes[-1].set_xlabel('Time [MJD]')
-fig.suptitle('PDS 110 Photometry', fontsize='large')
-
-fout = datetime.today().strftime('pds110_aavso_%Y%m%d_%H%M.png')
-plt.savefig(fout)
-plt.draw()
-
-
 tmax = 57974.
 
 fig, axes = plt.subplots(n_bands, 1, figsize=(8, 11), sharex=True, sharey=True)
                          # subplot_kw={'xticks': [], 'yticks': []})
 
 fig.subplots_adjust(hspace=0.05, wspace=0.05)
+
+# add expected location of eclipse
+from astropy.modeling.models import Gaussian1D
+#     .. math:: f(x) = A e^{- \frac{\left(x - x_{0}\right)^{2}}{2 \sigma^{2}}}
+# 0.5 = exp-x*x / (2*s*s)
+# -2 s s ln 0.5 = 1
+# s = -1/(2 ln 2) 
+# HJD=2458015.5 ± 10 (Sept 9-30 2017
+hjd_mid = 2458015.5 - 2400000.5
+ecl_fwhm = 7.
+ecl_sig = ecl_fwhm / np.sqrt((2*np.log(2)))
+f = Gaussian1D(0.26, hjd_mid, ecl_sig)
+
+t_ecl=np.arange(hjd_mid - 50, hjd_mid + 50, 1)
 
 
 for (ax, band) in zip(axes, ta_by_band.groups.keys):
@@ -129,12 +102,14 @@ for (ax, band) in zip(axes, ta_by_band.groups.keys):
 
 #        ax.errorbar(tc['MJD'], tc['Magnitude'], tc['Uncertainty'], fmt=conv[nob[0]], color=obscol[nob[0]])
         ax.errorbar(tc['MJD'], tc['I'], tc['Uncertainty'], fmt=conv[nob[0]], color=obscol[nob[0]])
-        ax.text(0.1, 0.9, band[0], ha='center', va='center', transform=ax.transAxes)
-        ax.vlines(now.mjd, 0.9, 1.1,linestyle='dashed')
+        ax.text(0.9, 0.2, band[0], ha='center', va='center', fontsize=24, transform=ax.transAxes)
+        ax.vlines(now.mjd, 0.0, 1.1,linestyle='dashed')
         ax.hlines(1.0, 0,60000,linestyle='dotted')
+        ax.plot(t_ecl, 1 - f(t_ecl))
 
-ax.set_ylim(0.88,1.12)
-ax.set_xlim(now.mjd-20, now.mjd+1)
+        
+ax.set_ylim(0.50,1.08)
+ax.set_xlim(now.mjd-20, now.mjd+40)
 
 axes[-1].set_xlabel('Time [MJD]')
 fig.suptitle('PDS 110 Photometry', fontsize='large')
@@ -143,3 +118,5 @@ fout = datetime.today().strftime('pds110_intens_aavso_%Y%m%d_%H%M.png')
 plt.savefig(fout)
 plt.draw()
 plt.show()
+print(t_ecl)
+print(f(t_ecl))
